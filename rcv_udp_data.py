b8a8cc5c1ef897fcc7b229493c44bc581911c1a0
yabool2001 <mzemlo.pl@gmail.com>
2023-03-04 14:00:56 +0100

    aaaaa

--- rcv_udp_data.py
+++ rcv_udp_data.py
@@ -23,26 +23,20 @@ src_udp_ip                      = socket.gethostbyname ( socket.gethostname () )
 ctrl_udp_port                   = 10004
 data_udp_port                   = 10005
 control                         = "ping"
-udp_ctrl_listening              = 0
+udp_data_listening              = 1
 
 hello = "\n\n##########################################\n############# mmradar started ############\n##########################################\n"
 
-def data_udp_ctrl_rx_thread () :
-    global control
-    global udp_ctrl_listening
-    src_udp_ctrl_rx = socket.socket ( socket.AF_INET , socket.SOCK_DGRAM )
-    src_udp_ctrl_rx.bind ( ( src_udp_ip , ctrl_udp_port ) )
-    try :
-        udp_ctrl_listening = 1 # Na wszelki wypadek równolegle z identyczną zmianą poza thread
-        ctrl , address = src_udp_ctrl_rx.recvfrom ( 4096 )
-        pprint.pprint ( ctrl.decode() )
-        pprint.pprint ( address[0] )
-        control = ctrl.decode()
-        src_udp_ctrl_rx.close ()
-    except struct.error as e :
-        print ( e )
-    udp_ctrl_listening = 0
-    #udp.sendto ( str.encode ( str ( pc3d_object.frame_dict ) , "utf-8" ) , ( dst_udp_ip , data_udp_port ) )
+def data_udp_rx_thread () :
+    while True :
+        udp_data_rx = socket.socket ( socket.AF_INET , socket.SOCK_DGRAM )
+        udp_data_rx.bind ( ( src_udp_ip , data_udp_port ) )
+        try :
+            ctrl , address = udp_data_rx.recvfrom ( 4096 )
+            pprint.pprint ( ctrl.decode() )
+            pprint.pprint ( address[0] )
+        except struct.error as e :
+            print ( e )
 
 ################################################################
 ################ SOCKET Configuration ##########################
@@ -61,21 +55,13 @@ def data_udp_ctrl_rx_thread () :
 #        pprint.pprint ( address[0] )
 #    except struct.error as e :
 #        print ( e )
-if udp_ctrl_listening == 0 :
-    thread_udp_ctrl_rx = threading.Thread ( target = data_udp_ctrl_rx_thread )
-    thread_udp_ctrl_rx.start ()
-    udp_ctrl_listening = 1 # Na wszelki wypadek równolegle z identyczną zmianą wew. thread
-start_t = time.perf_counter ()
+if udp_data_listening :
+    data_udp_rx = threading.Thread ( target = data_udp_rx_thread )
+    data_udp_rx.start ()
+
 while True :
-    if udp_ctrl_listening == 0 :
-        thread_udp_ctrl_rx = threading.Thread ( target = data_udp_ctrl_rx_thread )
-        thread_udp_ctrl_rx.start ()
-        udp_ctrl_listening = 1 # Na wszelki wypadek równolegle z identyczną zmianą wew. thread
-    finish_t = time.perf_counter ()
-    if finish_t - start_t > 2 :
-        print ( control )
-        start_t = time.perf_counter ()
+    pass
 
 ################# CLOSE DATA COM PORT FILE ######################
 #src_udp_data_rx.close ()
-src_udp_ctrl_rx.close ()
+#src_udp_ctrl_rx.close ()
